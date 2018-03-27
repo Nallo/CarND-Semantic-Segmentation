413691014664237bf5abb7354f2a73c2c1fc074c
Stefano Martinallo <stefano@sherlock.bike>
2018-03-27 21:34:38 +0200

    Epocs and time
    Signed-off-by: Stefano Martinallo <stefano@sherlock.bike>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os.path
 import tensorflow as tf
 import helper
 import warnings
+import time
 from distutils.version import LooseVersion
 import project_tests as tests
 
@@ -171,7 +172,7 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
-        epochs = 1
+        epochs = 50
         batch_size = 5
 
         # TODO: Build NN using load_vgg, layers, and optimize function
@@ -182,13 +183,16 @@ def run():
         logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        t0 = time.time()
         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
+        t1 = time.time()
 
         # TODO: Save inference data using helper.save_inference_samples
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
+        print("Trained in {}s".format(int(t1-t0)))
 
 if __name__ == '__main__':
     run()
